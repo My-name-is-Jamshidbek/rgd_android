0f283c6d21b6d28857b661b24c4dbc6f14cf099d
Jamshidbek <jamshidbekollanazarov@gmail.com>
2024-07-20 08:08:14 +0500

    listview added

---
+++ app/src/main/java/com/example/waterfilter/data/User.kt
@@ -0,0 +1,9 @@
+package com.example.waterfilter.data
+
+data class User(
+    val fullName: String,
+    val phone: String,
+    val address: String,
+    val latitude: Double,
+    val longitude: Double
+)
